2f6f06aef56d0684c88793a3d1420f4b8b84be09
jiseo.jeong <jisxo@kakao.com>
2026-02-22 15:07:20 +0900

    refactor: 일반화 ML 데이터 엔지니어 포트폴리오 정리

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,16 +1,16 @@
 import { Project, StarContent } from './projects/types';
 import { cryptoStreamingProject } from './projects/crypto_streaming';
-import { retailGnnProject } from './projects/retail_gnn';
 import { securityPlatformPhasedProject } from './projects/security_platform_phased';
 import { telecomProject } from './projects/telecom';
 import { hiddenSpotMinioProject } from './projects/hidden_spot_minio';
+import { retailGnnProject } from './projects/retail_gnn';
 
 export type { Project, StarContent };
 
 export const projects: Project[] = [
     cryptoStreamingProject,
-    telecomProject,
     securityPlatformPhasedProject,
-    retailGnnProject,
-    hiddenSpotMinioProject
+    telecomProject,
+    hiddenSpotMinioProject,
+    retailGnnProject
 ];
